be1fa11e6594975085841f0863a379acc0f6a03e
Eduardo <eduardodmdev@icloud.com>
2023-02-20 23:47:11 +0100

    Initial Commit

---
+++ SwiftChartsInAction/SwiftChartsInActionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChartsInActionApp.swift
+//  SwiftChartsInAction
+//
+//  Created by Eduardo Developer on 20/2/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChartsInActionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
