bfc6d586e50ea4a7ff723967fae440a44a6f346f
Eduardo <eduardodmdev@icloud.com>
2023-06-11 08:50:08 +0200

    Code Basis graphic

--- SwiftChartsInAction/EvolutionTopFiveEmittersView.swift
+++ SwiftChartsInAction/EvolutionTopFiveEmittersView.swift
@@ -6,25 +6,28 @@
 //
 
 import SwiftUI
+import Charts
 
 struct EvolutionTopFiveEmittersView: View {
     
     @EnvironmentObject var store: DataStore
     
     var body: some View {
-            List{
-                ForEach(store.evolutionTopFiveEmitters) { country in
-                    Text("\(country.name)")
-                }
-                //  Text("\($0.name)")
-            
+        Chart(store.evolutionTopFiveEmitters) { country in
+            AreaMark(
+                x: .value("Year", country.year),
+                y: .value("Global", country.total ?? 0)
+            )
+            .foregroundStyle(
+                by: .value("Country", country.name)
+            )
         }
     }
 }
-/*
+
 struct EvolutionTopFiveEmittersView_Previews: PreviewProvider {
     static var previews: some View {
         EvolutionTopFiveEmittersView()
     }
 }
-*/
+
