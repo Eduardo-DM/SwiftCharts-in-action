c1b579a571a9a2d8f7e0d5572dd58f443a40886e
Eduardo <eduardodmdev@icloud.com>
2023-06-19 09:03:24 +0200

    Set vainilla stacked top 5 emitters

--- SwiftChartsInAction/EvolutionTopFiveEmittersStackedView.swift
+++ SwiftChartsInAction/EvolutionTopFiveEmittersStackedView.swift
@@ -19,21 +19,12 @@ struct EvolutionTopFiveEmittersStackedView: View {
         Chart(store.evolutionTopFiveEmitters.filter({$0.year>=startYear})) { country in
             AreaMark(
                 x: .value("Year", country.year),
-                y: .value("Global", country.total ?? 0),
+                y: .value("Total emissions", country.total ?? 0),
                 stacking: .standard
             )
             .foregroundStyle(
                 by: .value("Country", country.name)
             )
-        }
-        .chartXScale(domain: startYear...2021)
-        .chartYScale(domain: 0...23000)
-        .chartXAxis {
-            AxisMarks() { _ in
-                AxisGridLine()
-                AxisValueLabel(centered: false, collisionResolution: .disabled)
-                    .offset(x: -10.0, y:1.0)
-            }
         }
         .padding()
     }
@@ -45,3 +36,12 @@ struct EvolutionTopFiveEmittersStackedView_Previews: PreviewProvider {
             .environmentObject(DataStore())
     }
 }
+/*    .chartXScale(domain: startYear...2021)
+    .chartYScale(domain: 0...23000)
+    .chartXAxis {
+        AxisMarks() { _ in
+            AxisGridLine()
+            AxisValueLabel(centered: false, collisionResolution: .disabled)
+                .offset(x: -10.0, y:1.0)
+        }
+    }*/
